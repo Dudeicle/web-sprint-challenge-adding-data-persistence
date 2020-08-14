eaf47304a973996f04045b99386052c462b8e86a
Dudeicle <brian-griffiths@lambdastudents.com>
2020-08-14 12:37:41 -0400

    all get requests are working plus getbyID

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,3 +1,38 @@
 const db = require("../data/db-config.js");
 
-module.exports = {};
+module.exports = {
+	getProjects,
+	getProjectByID,
+	addProject,
+	getTasks,
+	addTask,
+	getResources,
+	addResource,
+};
+
+// SIMPLE GET REQUESTS
+function getProjects() {
+	return db("projects");
+} // WORKING
+function getTasks() {
+	return db("tasks");
+} // WORKING
+function getResources() {
+	return db("resources");
+} // WORKING
+
+// GET PROJECT BY ID REQUEST
+function getProjectByID(id) {
+	return db("projects").where({ id }).first();
+}
+
+// ---------------------------
+function addProject() {
+	return db("resources");
+}
+function addTask() {
+	return db("resources");
+}
+function addResource() {
+	return db("resources");
+}
--- projects/project-router.js
+++ projects/project-router.js
@@ -1,7 +1,55 @@
 const express = require("express");
 
-const Recipe = require("./project-model.js");
+const Project = require("./project-model.js");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+	Project.getProjects()
+		.then((projects) => {
+			res.json(projects);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "Failed to get list of projects" });
+		});
+}); // WORKING
+
+router.get("/tasks", (req, res) => {
+	Project.getTasks()
+		.then((tasks) => {
+			res.json(tasks);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "Failed to get list of tasks" });
+		});
+}); // WORKING
+
+router.get("/resources", (req, res) => {
+	Project.getResources()
+		.then((resources) => {
+			res.json(resources);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "Failed to get list of resources" });
+		});
+}); // WORKING
+
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+
+	Project.getProjectByID(id)
+		.then((project) => {
+			if (project) {
+				res.json(project);
+			} else {
+				res
+					.status(404)
+					.json({ message: "Could not find single project of that id" });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "Failed to get single project" });
+		});
+}); // WORKING
 
 module.exports = router;
